2dc089dbf9e3a8eaf310c3c9abb2b2bf946e35a0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-05 02:41:00 -0600

    -- Setup video for Xcode, Hello world, Meta iOS Developer talked about the role and daily tasks and scope for an engineer

--- IntroToMobileDevelopmentMetaCert/ContentView.swift
+++ IntroToMobileDevelopmentMetaCert/ContentView.swift
@@ -19,8 +19,12 @@ struct ContentView: View {
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
---
+++ IntroToMobileDevelopmentMetaCert/HelloWorldIntro.swift
@@ -0,0 +1,21 @@
+//
+//  HelloWorldIntro.swift
+//  IntroToMobileDevelopmentMetaCert
+//
+//  Created by Mitch Andrade on 8/5/23.
+//
+
+import SwiftUI
+
+struct HelloWorldIntro: View {
+    var body: some View {
+        Text("Hello world!")
+            .bold()
+    }
+}
+
+struct HelloWorldIntro_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloWorldIntro()
+    }
+}
